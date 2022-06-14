74e30be091a54f59b8673e107994cf05b2f265da
OLAWALE MARTINS <rytesoft@hotmail.com>
2022-06-14 15:50:59 +0100

    Add project files.

---
+++ Data/ClientDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppTest.Data
+{
+    public class ClientDetail
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ExampleModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppTest
+{
+    public class ExampleModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+    }
+    public class Starship
+    {
+        [Required]
+        [StringLength(16, ErrorMessage = "Identifier too long (16 character limit).")]
+        public string Identifier { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Classification { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Accommodation invalid (1-100000).")]
+        public int MaximumAccommodation { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true",
+            ErrorMessage = "This form disallows unapproved ships.")]
+        public bool IsValidatedDesign { get; set; }
+
+        [Required]
+        public DateTime ProductionDate { get; set; }
+    }
+}
